bdeded00f12edb3b8ba0020fa751cacc6bc15607
Swapnil Kawade <skawade@sequoiacap.com>
2024-03-20 12:38:45 -0700

    Update Faraday Adapter

--- lib/okta/jwt.rb
+++ lib/okta/jwt.rb
@@ -91,7 +91,7 @@ module Okta
     # init client
     def client(issuer)
       Faraday.new(url: issuer) do |f|
-        f.adapter Faraday::Adapter::NetHttp
+        f.adapter Faraday.default_adapter
         f.headers['Accept'] = 'application/json'
       end
     end
